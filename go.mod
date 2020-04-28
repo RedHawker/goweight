a099b49b11914d5f2198e3593f693b25449b96f8
bmerle <bmerle@scires.com>
2020-04-28 19:07:05 -0400

    Initial changes to try and get a more correct binary size, along with more output control options

--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
 module github.com/RedHawker/goweight
 
 require (
-	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
-	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/dustin/go-humanize v0.0.0-20180713052910-9f541cc9db5d
 	github.com/mattn/go-zglob v0.0.0-20180803001819-2ea3427bfa53
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.2
-	github.com/stretchr/testify v1.2.2 // indirect
 	github.com/thoas/go-funk v0.0.0-20180716193722-1060394a7713
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
+
+go 1.13
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dustin/go-humanize"
+	"strings"
 
-	"github.com/jondot/goweight/pkg"
+	"github.com/RedHawker/goweight/pkg"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -16,10 +18,29 @@ var (
 )
 
 var (
-	jsonOutput = kingpin.Flag("json", "Output json").Short('j').Bool()
+	// jsonOutput overrides other output options
+	jsonOutput = kingpin.Flag("json", "Output json (overrides other output options)").Short('j').Bool()
+
+	byteOutput = kingpin.Flag("bytes", "Output size in bytes, instead of human-readable").Short('b').Default("false").Bool()
+	excludeInternal = kingpin.Flag("exclude-internal", "Exclude Go internal packages").Short('x').Default("false").Bool()
+	includeSum = kingpin.Flag("sum", "Include sum line in output").Short('s').Default("false").Bool()
+
 	buildTags  = kingpin.Flag("tags", "Build tags").String()
 	packages   = kingpin.Arg("packages", "Packages to build").String()
 )
+
+// Grabbed from vcs.go, don't know if it's perfect for what I'm trying to achieve
+func isInternal(importPath string) (bool) {
+	slash := strings.Index(importPath, "/")
+	if slash < 0 {
+		slash = len(importPath)
+	}
+	host := importPath[:slash]
+	if !strings.Contains(host, ".") {
+		return true
+	}
+	return false
+}
 
 func main() {
 	kingpin.Version(fmt.Sprintf("%s (%s)", version, commit))
@@ -39,8 +60,26 @@ func main() {
 		m, _ := json.Marshal(modules)
 		fmt.Print(string(m))
 	} else {
+		var total uint64 = 0
 		for _, module := range modules {
-			fmt.Printf("%8s %s\n", module.SizeHuman, module.Name)
+			if module.Name != "runtime" && *excludeInternal {
+				if isInternal(module.Name) {
+					continue
+				}
+			}
+			total += module.Size
+			if !*byteOutput {
+				fmt.Printf("%8s %s\n", module.SizeHuman, module.Name)
+			} else {
+				fmt.Printf("%10d %s\n", module.Size, module.Name)
+			}
+		}
+		if *includeSum {
+			if !*byteOutput {
+				fmt.Printf("%8s Sum\n", humanize.Bytes(total))
+			} else {
+				fmt.Printf("%10d Sum\n", total)
+			}
 		}
 	}
 }
